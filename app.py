aa8a04fdf66dade2a9883816b578eb34a0b25be9
omborate12 <omborate12@gmail.com>
2023-10-01 15:49:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 import numpy as np
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app,origins='http://localhost:3000')
 ## load pickle model
 model = pickle.load(open("model.pkl","rb"))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "Sucessfully deployed "
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    float_feature = [float(x) for x in request.form.values()]
-    features = [np.array(float_feature)]
+    data = request.get_json()
+    no_city = data.get('city_expansion')
+    market_size = data.get('market_size')
+    sales_prev_years =data.get('sale_prev_year')
+    sales_prev_month = data.get('sale_prev_month')
+    profit = data.get('profit')
+    ebidta = data.get('ebidta')
+    gross_margin = data.get('gross_margin')
+    amount_for_equity = data.get('amount_for_equity')
+    equity_ask = data.get('equity_ask')
+    valuation = data.get('valuation')
+    features = [ no_city, market_size, sales_prev_years, sales_prev_month, profit,ebidta, gross_margin, amount_for_equity,equity_ask, valuation]
+    for x in features:
+        print(int(x),x)
+    
+    float_features = [float(x) for x in features]
+    features = [np.array(float_features)]
     prediction = model.predict(features)
-    print(prediction)
-    val = prediction[0]
+    val = prediction[0]    
+    
+    # float_feature = [float(x) for x in features1]
+    # features = [np.array(features)]
+    # prediction = model.predict(features)
+    # print(prediction)
+    # val = prediction[0]
     print(val)
+    # result = "Startup will become successful in coming years" if prediction[0] == 1 else "Startup will not become successful in coming years"
+    if prediction[0] == 1: 
+        result = "Startup will become successful";
+    else:
+        result = "Startup will not become successful"    
+
+    # print(result)
+    return result
     if val == True:
         return render_template('index.html' , value = "Startup will become successful in coming years")
     else:
--- model.py
+++ model.py
@@ -3,11 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 
-df= pd.read_csv('StartupVal.csv')
-df.nunique(axis=0)
+df= pd.read_csv('StartupData.csv')
+# df.nunique(axis=0)
 
-x = df.drop(['Acquired','Domain'],axis=1)
-y= df['Acquired']
+x = df.drop(['got_offer(1/0)'],axis=1)
+y= df['got_offer(1/0)']
 
 from sklearn.model_selection import train_test_split
 x_train, x_test , y_train ,y_test = train_test_split(x,y,test_size = 0.2,random_state = 0)
@@ -18,7 +18,7 @@ x_train= st_x.fit_transform(x_train)
 x_test= st_x.transform(x_test)
 
 from sklearn.neighbors import KNeighborsClassifier
-classifier= KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2 )
+classifier= KNeighborsClassifier(n_neighbors=6, metric='minkowski', p=2 )
 classifier.fit(x_train, y_train)
 
 ##picke file
